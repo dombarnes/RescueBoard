b033dcb904addbd4899405c0e0d20b1e4fbdeb09
Dom Barnes <dom@dombarnes.com>
2015-05-14 12:32:04 +0100

    adding all products

--- config/products.rb
+++ config/products.rb
@@ -6,8 +6,11 @@ configure do
 	# The :id is the App Figures product ID. You can fetch this at https://api.appfigures.com/v2/products/mine
 	# The :color can be red, blue, green, yellow, orange, purple, aqua, or pink
 	set :products, [
-					  { :title => "TTV", :id => 36376562723, :color => "green" },
-					  { :title => "BookClub", :id => 40014607864, :color => "yellow" },
-					  { :title => "Snappt", :id => 40304598815, :color => "red" }
-					]
+			{ :title => "Trilby Training", :id => 5954125798, :color => "aqua" },
+    		{ :title => "TTV", :id => 36376562723, :color => "green" },
+    		# { :title => "TTV Player", :id => 36376203818, :color => "red" },
+    		{ :title => "BookClub", :id => 40014607864, :color => "yellow" },
+    		{ :title => "Supporting Teachers", :id => 40189399639, :color => "blue" },
+    		{ :title => "Snappt", :id => 40304598815, :color => "red" }					
+    	]
 end
