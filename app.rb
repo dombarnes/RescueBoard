93f367ac4fdec0636e0140793181c63e9e619d4e
Dom Barnes <dom@dombarnes.com>
2015-08-31 17:39:22 +0100

    Updating gems

--- app.rb
+++ app.rb
@@ -88,3 +88,30 @@ get '/appstore/downloads' do
 	end
 	build_salesgraph(datasequences,minTotal, maxTotal, type).to_json
 end
+
+get '/appstore/sales/:app_id' do |n|
+	raise Exception.new("Please specify AF_USERNAME in your environment") if settings.af_username.nil?
+	raise Exception.new("Please specify AF_PASSWORD in your environment") if settings.af_password.nil?
+	raise Exception.new("Please specify AF_CLIENT_KEY in your environment") if settings.af_client_key.nil?
+
+	months = { "1" => "Jan", "2" => "Feb", "3" => "Mar", "4" => "Apr", "5" => "May", "6" => "Jun", "7" => "Jul", "8" => "Aug", "9" => "Sep", "10" => "Oct", "11" => "Nov", "12" => "Dec" }
+	startDate = (Date.today - days.to_i).strftime("%Y-%m-%d")
+	endDate = (Date.today - 1).strftime("%Y-%m-%d")
+	datasequences = []
+	
+	salesData = []
+	response = HTTParty.get("https://api.appfigures.com/v2/reports/sales/?group_by=product&start_date=${startDate}&end_date=${endDate}&granularity=year&products=#{n}&dataset=none&include_inapps=true&format=json", :headers => { "X-Client-Key" => settings.af_client_key}, :basic_auth => {:username => settings.af_username, :password => settings.af_password })
+	response.parsed_response.sort.each do |day|
+	    day_hash = day[1]
+	    day_hash.each do |data|
+	        newDate = Date.parse(data[1]['date'])
+            dateString = "#{newDate.day} #{months["#{newDate.month}"]}"
+            downloads = comma_numbers(data[1]["downloads"].to_i)
+            maxTotal = downloads.to_i if downloads.to_i > maxTotal
+            minTotal = downloads.to_i if downloads.to_i < minTotal || minTotal == 1
+            salesData << { :title => dateString, :value => downloads }
+            lastDate = dateString
+        end
+    end
+    datasequences << { :title => p[:title], :color => p[:color], :datapoints => salesData }
+end
